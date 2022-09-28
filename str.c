174197bbda1344b8b523308299d351d226bc2c6e
Michael Clift <michael.j.clift@gmail.com>
2022-09-28 14:25:40 +1000

    some top level wrappers didn't need to be wrappers

--- str.c
+++ str.c
@@ -21,13 +21,8 @@
 	static void change_buf_capacity(str_buf_t* buf_ptr, size_t new_capacity);
 	static void assign_str_to_buf(str_buf_t* buf_ptr, str_t str);
 	static void append_char_to_buf(str_buf_t* str_buf, char c);
-	static str_t substring(str_t str, int begin, int end);
-	static str_t trim_string(str_t str, str_t chars_to_trim);
 	static str_search_result_t find_first_needle(str_t haystack, str_t needle);
-	static str_search_result_t find_last_needle(str_t haystack, str_t needle);
 	static bool str_contains_char(str_t str, char c);
-	static str_t pop_first_split(str_t* str_ptr, str_t delimiters);
-	static str_t pop_last_split(str_t* str_ptr, str_t delimiters);
 	static size_t round_up_capacity(size_t capacity);
 	static str_t str_of_buf(str_buf_t* buf);
 
@@ -100,12 +95,40 @@ bool str_contains(str_t haystack, str_t needle)
 
 str_t str_sub(str_t str, int begin, int end)
 {
-	return substring(str, begin, end);
+	str_t result = (str_t){.size = 0, .data = str.data};
+
+	if(str.data && str.size)
+	{
+		if(begin < 0)
+			begin = str.size + begin;
+		if(end < 0)
+			end = str.size + end;
+
+		if(begin >= str.size)		//begin is inclusive, so must be < size
+			begin = str.size-1;
+		if(end > str.size)			//end is non-inclusive, so must be <= size
+			end = str.size;
+
+		result.data = &str.data[begin];
+		result.size = end-begin;
+	};
+
+	return result;
 }
 
 str_t str_trim(str_t str, str_t chars_to_trim)
 {
-	return trim_string(str, chars_to_trim);
+	while(str_contains_char(chars_to_trim, *str.data) && str.size)
+	{
+		str.data++;
+		str.size--;
+	};
+	while(str_contains_char(chars_to_trim, str.data[str.size]) && str.size)
+	{
+		str.data--;
+		str.size--;
+	};
+	return str;
 }
 
 str_search_result_t str_find_first(str_t haystack, str_t needle)
@@ -115,17 +138,87 @@ str_search_result_t str_find_first(str_t haystack, str_t needle)
 
 str_search_result_t str_find_last(str_t haystack, str_t needle)
 {
-	return find_last_needle(haystack, needle);
+	str_search_result_t result = (str_search_result_t){.found=false, .index=0};
+
+	const char* remaining_hay = &haystack.data[haystack.size - needle.size];
+
+	if(haystack.data && needle.data)
+	{
+		while((remaining_hay >= haystack.data) && !result.found)
+		{
+			result.found = !memcmp(remaining_hay, needle.data, needle.size);
+			remaining_hay -= !result.found;
+		};
+	};
+
+	if(result.found)
+		result.index = remaining_hay - haystack.data;
+
+	return result;
 }
 
 str_t str_pop_first_split(str_t* str_ptr, str_t delimiters)
 {
-	return pop_first_split(str_ptr, delimiters);
+		str_t result = (str_t){.data = NULL, .size = 0};
+	bool found = false;
+	const char* ptr = str_ptr->data;
+
+	if(str_ptr->data && delimiters.data)
+	{
+		// trt to find the delimiter
+		while(ptr != &str_ptr->data[str_ptr->size] && !found)
+		{
+			found = str_contains_char(delimiters, *ptr);
+			ptr += !found;
+		};
+	};
+
+	if(found)
+	{
+		result.data = str_ptr->data;
+		result.size = ptr - str_ptr->data;
+		str_ptr->data = ptr;
+		str_ptr->size -= result.size;
+
+		// at this stage, the remainder still includes the delimiter
+		str_ptr->size--;
+		if(str_ptr->size)	//only point the the character after the delimiter if there is one
+			str_ptr->data++;
+	};
+
+	return result;
 }
 
 str_t str_pop_last_split(str_t* str_ptr, str_t delimiters)
 {
-	return pop_last_split(str_ptr, delimiters);
+	str_t result = (str_t){.data = NULL, .size = 0};
+	bool found = false;
+	const char* ptr;
+
+	if(str_ptr->data && str_ptr->size && delimiters.data)
+	{
+		// starting from the last character, try to find the delimiter backwards
+		ptr = &str_ptr->data[str_ptr->size-1];
+		while(ptr != str_ptr->data-1 && !found)
+		{
+			found = str_contains_char(delimiters, *ptr);
+			ptr -= !found;
+		};
+	};
+
+	if(found)
+	{
+		result.data = ptr;
+		result.size = &str_ptr->data[str_ptr->size] - ptr;
+		str_ptr->size -= result.size;
+
+		// at this stage, the result still starts with the delimiter
+		result.size--;
+		if(result.size)
+			result.data++; //only point the the character after the delimiter if there is one
+	};
+
+	return result;
 }
 
 //********************************************************************************************************
@@ -204,44 +297,6 @@ static void assign_str_to_buf(str_buf_t* buf_ptr, str_t str)
 	append_str_to_buf(buf_ptr, str);
 }
 
-static str_t substring(str_t str, int begin, int end)
-{
-	str_t result = (str_t){.size = 0, .data = str.data};
-
-	if(str.data && str.size)
-	{
-		if(begin < 0)
-			begin = str.size + begin;
-		if(end < 0)
-			end = str.size + end;
-
-		if(begin >= str.size)		//begin is inclusive, so must be < size
-			begin = str.size-1;
-		if(end > str.size)			//end is non-inclusive, so must be <= size
-			end = str.size;
-
-		result.data = &str.data[begin];
-		result.size = end-begin;
-	};
-
-	return result;
-}
-
-static str_t trim_string(str_t str, str_t chars_to_trim)
-{
-	while(str_contains_char(chars_to_trim, *str.data) && str.size)
-	{
-		str.data++;
-		str.size--;
-	};
-	while(str_contains_char(chars_to_trim, str.data[str.size]) && str.size)
-	{
-		str.data--;
-		str.size--;
-	};
-	return str;
-}
-
 static str_search_result_t find_first_needle(str_t haystack, str_t needle)
 {
 	str_search_result_t result = (str_search_result_t){.found=false, .index=0};
@@ -259,91 +314,6 @@ static str_search_result_t find_first_needle(str_t haystack, str_t needle)
 
 	if(result.found)
 		result.index = remaining_hay - haystack.data;
-
-	return result;
-}
-
-static str_search_result_t find_last_needle(str_t haystack, str_t needle)
-{
-	str_search_result_t result = (str_search_result_t){.found=false, .index=0};
-
-	const char* remaining_hay = &haystack.data[haystack.size - needle.size];
-
-	if(haystack.data && needle.data)
-	{
-		while((remaining_hay >= haystack.data) && !result.found)
-		{
-			result.found = !memcmp(remaining_hay, needle.data, needle.size);
-			remaining_hay -= !result.found;
-		};
-	};
-
-	if(result.found)
-		result.index = remaining_hay - haystack.data;
-
-	return result;
-}
-
-static str_t pop_first_split(str_t* str_ptr, str_t delimiters)
-{
-	str_t result = (str_t){.data = NULL, .size = 0};
-	bool found = false;
-	const char* ptr = str_ptr->data;
-
-	if(str_ptr->data && delimiters.data)
-	{
-		// trt to find the delimiter
-		while(ptr != &str_ptr->data[str_ptr->size] && !found)
-		{
-			found = str_contains_char(delimiters, *ptr);
-			ptr += !found;
-		};
-	};
-
-	if(found)
-	{
-		result.data = str_ptr->data;
-		result.size = ptr - str_ptr->data;
-		str_ptr->data = ptr;
-		str_ptr->size -= result.size;
-
-		// at this stage, the remainder still includes the delimiter
-		str_ptr->size--;
-		if(str_ptr->size)	//only point the the character after the delimiter if there is one
-			str_ptr->data++;
-	};
-
-	return result;
-}
-
-static str_t pop_last_split(str_t* str_ptr, str_t delimiters)
-{
-	str_t result = (str_t){.data = NULL, .size = 0};
-	bool found = false;
-	const char* ptr;
-
-	if(str_ptr->data && str_ptr->size && delimiters.data)
-	{
-		// starting from the last character, try to find the delimiter backwards
-		ptr = &str_ptr->data[str_ptr->size-1];
-		while(ptr != str_ptr->data-1 && !found)
-		{
-			found = str_contains_char(delimiters, *ptr);
-			ptr -= !found;
-		};
-	};
-
-	if(found)
-	{
-		result.data = ptr;
-		result.size = &str_ptr->data[str_ptr->size] - ptr;
-		str_ptr->size -= result.size;
-
-		// at this stage, the result still starts with the delimiter
-		result.size--;
-		if(result.size)
-			result.data++; //only point the the character after the delimiter if there is one
-	};
 
 	return result;
 }
