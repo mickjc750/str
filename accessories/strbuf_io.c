3f34d153bb8da5e9bbfe7e222f94e758abc573ed
mick <michael.j.clift@gmail.com>
2025-08-26 11:08:52 +1000

    strbuf_append_file()

--- accessories/strbuf_io.c
+++ accessories/strbuf_io.c
@@ -2,8 +2,9 @@
 */
 	#include <limits.h>
 	#include <unistd.h>
+	#include <fcntl.h>
 	#include "strbuf_io.h"
-	#include "strview.h"
+	#include "strbuf.h"
 
 //********************************************************************************************************
 // Local defines
@@ -57,6 +58,52 @@ int strbuf_write(int fd, strbuf_t** buf_ptr)
 
 		*buf_ptr = buf;
 	};
+
+	return retval;
+}
+
+strview_t strbuf_append_file(strbuf_t **dst, const char* file_name)
+{
+	int fd = -1;
+	int err;
+	bool failed;
+	bool eof = false;
+	int resize;
+	strview_t retval = STRVIEW_INVALID;
+
+	failed = (dst == NULL || *dst == NULL);
+
+	if(!failed)
+	{
+		fd = open(file_name, O_RDONLY);
+		failed = (fd == -1);
+	};
+
+	while(!failed && !eof)
+	{
+		err = strbuf_append_read(dst, fd);
+		failed = (err == -1);
+		eof = (err == 0);
+		if(!failed && !eof)
+		{
+			resize = (*dst)->capacity;
+			if(resize < (INT_MAX-2)/2)
+				resize *= 2;
+			else
+				resize = INT_MAX-1;
+			failed = !(resize > (*dst)->capacity);
+		};
+		if(!failed && !eof)
+			failed = !strview_is_valid(strbuf_grow(dst, resize));
+	};
+	
+	if(fd != -1)
+		close(fd);
+
+	if(eof)
+		retval = strbuf_shrink(dst);
+	else
+		retval = strbuf_assign(dst, cstr(""));
 
 	return retval;
 }
--- accessories/strbuf_io.h
+++ accessories/strbuf_io.h
@@ -25,7 +25,7 @@
  * @note The return value is that returned by read(), read will always be called even if remaining space in the buffer is 0.
  * @note Does not increase the buffers capacity. Use strbuf_grow() to suitably size the buffer first.
    **********************************************************************************/
-	int strbuf_append_read(strbuf_t** buf_ptr, int fd);
+	int strbuf_append_read(strbuf_t **buf_ptr, int fd);
 
 /**
  * @brief Attempt to write the contents of the buffer using a POSIX write() and remove the number of bytes written.
@@ -33,6 +33,14 @@
  * @return The number of bytes written, or -1 for error with errno set.
  * @note The return value is that returned by write(), write() will always be called even if the buffer is empty.
    **********************************************************************************/
-	int strbuf_write(int fd, strbuf_t** buf_ptr);
+	int strbuf_write(int fd, strbuf_t **buf_ptr);
+
+/**
+ * @brief Append to a buffer from a file on disk.
+ * @param buf_ptr The address of a pointer to the target buffer.
+ * @return A view of the resulting buffer contents, or STRVIEW_INVALID if the operation failed.
+ * @note The buffer will be resized up to a maximum of INT_MAX-1 to allow the entire file to be appended.
+   **********************************************************************************/
+	strview_t strbuf_append_file(strbuf_t **buf_ptr, const char* file_name);
 
 #endif
